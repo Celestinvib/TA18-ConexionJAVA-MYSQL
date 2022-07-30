07eb37b0f8b1cbd245b4d4c4dd87369c7db65d52
Celestinvib <celestinvib@gmail.com>
2022-07-30 16:14:48 +0200

    Ejercicio2 v1.0

--- Ejercicio02/src/mainApp.java
+++ Ejercicio02/src/mainApp.java
@@ -10,105 +10,49 @@ import javax.swing.JOptionPane;
 
 import com.mysql.cj.MysqlConnection;
 
+import SQLConnection.SQLConnection;
+
 public class mainApp {
-	
-	public static Connection conexion;
-	
+		
 	public static void main(String[] args) {
 		
-		makeConection();
-		createDB("TA18_Ejercicio02");
-		createTable("TA18_Ejercicio02","CREATE TABLE Departamentos (Codigo INT PRIMARY KEY AUTO_INCREMENT, Nombre VARCHAR (50) , Presupuesto INT)");
-		insertData("TA18_Ejercicio02","INSERT INTO Departamentos (Nombre,Presupuesto) VALUE ('IT',80000);");		
-		insertData("TA18_Ejercicio02","INSERT INTO Departamentos (Nombre,Presupuesto) VALUE ('Research',50000);");		
-		insertData("TA18_Ejercicio02","INSERT INTO Departamentos (Nombre,Presupuesto) VALUE ('Accounting',44000);");		
-		insertData("TA18_Ejercicio02","INSERT INTO Departamentos (Nombre,Presupuesto) VALUE ('Human Resourcing',70000);");		
-		insertData("TA18_Ejercicio02","INSERT INTO Departamentos (Nombre,Presupuesto) VALUE ('Technology',20000);");		
+		SQLConnection conexion = new SQLConnection();			
 		
-		JOptionPane.showMessageDialog(null,"Datos almacenos correctamente");
-
-		createTable("TA18_Ejercicio02","CREATE TABLE Empleados (DNI VARCHAR(8) PRIMARY KEY, Nombre VARCHAR (100) , Apellidos VARCHAR (255), Departamento INT NULL, CONSTRAINT departamento FOREIGN KEY (Departamento) REFERENCES Departamentos(Codigo) ON DELETE SET NULL  ON UPDATE CASCADE)");
-		insertData("TA18_Ejercicio02","INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('46423897','Ceres','García Pérez','1');");		
-		insertData("TA18_Ejercicio02","INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('48425309','Jose','Piqué Chiesa','2');");
-		insertData("TA18_Ejercicio02","INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('41539450','Clara','Ruiz Ruiz','3');");	
-		insertData("TA18_Ejercicio02","INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('48391395','Aroa','Mendoza','4');");	
-		insertData("TA18_Ejercicio02","INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('41277319','Mark','Garcia','5');");	
+		structureCreation(conexion);
 		
-		JOptionPane.showMessageDialog(null,"Datos almacenos correctamente");
-
-		closeConnection();
-		
+		conexion.closeConnection();		
 	}
 	
-	public static void makeConection() {
-	    String ip = JOptionPane.showInputDialog(null,"Introduce los datos de la conexión sql\nIp:"); 
+	/**
+	* Metodo donde se realiza la creacion de la estructura mysql con datos staticos
+	*/
+	public static void structureCreation(SQLConnection conexion) {
+		
+		String dbName = "TA18_Ejercicio02";
+		
+		conexion.createDB(dbName); 
+		
+		conexion.createTable(dbName,"CREATE TABLE Departamentos (Codigo INT PRIMARY KEY , Nombre VARCHAR (50) , Presupuesto INT)");
+		
+		conexion.insertData(dbName,"INSERT INTO Departamentos (Codigo,Nombre,Presupuesto) VALUE ('1','IT',80000);");		
+		conexion.insertData(dbName,"INSERT INTO Departamentos (Codigo,Nombre,Presupuesto) VALUE ('2','Research',50000);");		
+		conexion.insertData(dbName,"INSERT INTO Departamentos (Codigo,Nombre,Presupuesto) VALUE ('3','Accounting',44000);");		
+		conexion.insertData(dbName,"INSERT INTO Departamentos (Codigo,Nombre,Presupuesto) VALUE ('4','Human Resourcing',70000);");		
+		conexion.insertData(dbName,"INSERT INTO Departamentos (Codigo,Nombre,Presupuesto) VALUE ('5','Technology',20000);");	
+		
+		
+		conexion.createTable(dbName,"CREATE TABLE Empleados (DNI VARCHAR(8) PRIMARY KEY, Nombre VARCHAR (100) , Apellidos VARCHAR (255), Departamento INT NULL, CONSTRAINT departamento FOREIGN KEY (Departamento) REFERENCES Departamentos(Codigo) ON DELETE SET NULL  ON UPDATE CASCADE)");
 
-	    String user = JOptionPane.showInputDialog(null,"Usuario: ");
+		conexion.insertData(dbName,"INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('46423897','Ceres','García Pérez','1');");		
+		conexion.insertData(dbName,"INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('48425309','Jose','Piqué Chiesa','2');");
+		conexion.insertData(dbName,"INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('41539450','Clara','Ruiz Ruiz','3');");	
+		conexion.insertData(dbName,"INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('48391395','Aroa','Mendoza','4');");	
+		conexion.insertData(dbName,"INSERT INTO Empleados (DNI,Nombre, Apellidos, Departamento) VALUE ('41277319','Mark','Garcia','5');");	
+			}
+	
+	
+	
 
-	    String pass = JOptionPane.showInputDialog(null,"Contraseña: ");
-	    
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			conexion = DriverManager.getConnection("jdbc:mysql://"+ip+":3306?useTimezone=true&serverTimezone=UTC",user,pass);
-			JOptionPane.showMessageDialog(null,"Server Connected");				
-		}catch (SQLException | ClassNotFoundException ex ) {
-			System.out.println("No se ha podido conectar con mi base de datos");
-			System.out.println(ex);
-		}
-	}
-	
-	public static void closeConnection() {
-		
-		try {
-			conexion.close();
-			JOptionPane.showMessageDialog(null, "Se ha finalizado la conexión con el servidor");
-		} catch (SQLException ex) {
-			JOptionPane.showMessageDialog(null, ex);
-		}
-	}
-	
-	public static void createDB(String name) {
-				
-		try {
-			String Query = "CREATE DATABASE "+name;
-			Statement st = conexion.createStatement();
-			st.executeUpdate(Query);
-			JOptionPane.showMessageDialog(null,"Se ha creado la base de datos "+name+ " de forma exitosa");					
-		}catch (SQLException ex) {
-			
-		}
-	}
-	
-	public static void createTable(String db,String SQLquery) {
-		
-		try {
-			String Querydb = "USE "+db+";";
-			Statement stdb = conexion.createStatement();
-			stdb.executeUpdate(Querydb);
-			Statement st = conexion.createStatement();
-			st.executeUpdate(SQLquery);
-			JOptionPane.showMessageDialog(null, "Tabla Creada con exito!");
-			
-		}catch (SQLException ex)  {
-			System.out.println(ex.getMessage());
-			System.out.println("Error creando tabla.");
-		}
-		
-	}
-	public static void insertData (String db, String SQLquery) {
-		
-		try {
-			String Querydb = "USE "+db+";";
-			Statement stdb = conexion.createStatement();
-			stdb.executeUpdate(Querydb);
-			
-			Statement st = conexion.createStatement();
-			st.executeUpdate(SQLquery);
-			
-		}catch (SQLException ex) {
-			JOptionPane.showMessageDialog(null, "Error en el almacenamiento");
-		}
-	}
 	
 	
 }
